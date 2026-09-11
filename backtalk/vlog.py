8e53cd3fd3335ab4eab0dc63b22abf773123643b
m477h411-M <m477h411@gmail.com>
2026-09-10 19:35:41 -0700

    Update vlog.py

--- backtalk/vlog.py
+++ backtalk/vlog.py
@@ -1,5 +1,4 @@
 # backtalk: talk to your Claude Code agent out loud.
-# Copyright (C) 2026 Jared Rhodenizer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
