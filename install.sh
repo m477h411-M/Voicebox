2857caba06479b29784a6fc89759e523088b141e
m477h411-M <m477h411@gmail.com>
2026-09-10 19:28:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # backtalk: talk to your Claude Code agent out loud.
-# Copyright (C) 2026 Jared Rhodenizer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
